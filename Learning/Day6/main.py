8ff3f2f9d0f1a87c8b409b8f3b3fcc7acc274a38
S0Lo <133913126+S0LoGame@users.noreply.github.com>
2024-03-25 19:16:26 +0200

    Create main.py
    https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json - Link for the App

---
+++ Learning/Day6/main.py
@@ -0,0 +1,95 @@
+#Exercitiul 1
+# def turn_around():
+#     turn_left()
+#     turn_left()
+# def turn_right():
+#     for i in range(1,4):
+#         turn_left()
+# def move_9():
+#     for i in range(1,10):
+#         move()
+
+# move_9()
+# turn_left()
+# move_9()
+# turn_left()
+# move_9()
+# turn_left()
+# move_9()
+
+#Exercitiul 2
+# def turn_right():
+#     for i in range(1,4):
+#         turn_left()
+
+# def final():
+#     for i in range(6):
+#         move()
+#         turn_left()
+#         move()
+#         turn_right()
+#         move()
+#         turn_right()
+#         move()
+#         turn_left()
+        
+# final()
+
+#Exercitiul 3
+# def turn_right():
+#     for i in range(1,4):
+#         turn_left()
+# def jump():
+#       turn_left()
+#       move()
+#       turn_right()
+#       move()
+#       turn_right()
+#       move()
+#       turn_left()
+
+# while not at_goal():
+#     while not wall_in_front():
+#         move()
+#     if front_is_clear() == False:
+#        jump()
+        
+#Exercitiul 4
+# def turn_right():
+#     for i in range(1,4):
+#         turn_left()
+
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+    
+    
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+#Exercitiul 5
+# def turn_right():
+#     for i in range(1,4):
+#         turn_left()
+# while front_is_clear():
+#     move()
+# turn_left()
+        
+# while not at_goal():
+#     if right_is_clear() == True:
+#         turn_right()
+#         move()
+#     elif front_is_clear() == True:
+#         move()
+#     else:
+#         turn_left()
