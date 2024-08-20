445116484d22f3bf3c293d4f73755254f5d1616e
S0LoGame <marius.movila15@gmail.com>
2024-08-20 10:58:36 +0300

    Upload Day23

---
+++ Learning/Day23/scoreboard.py
@@ -0,0 +1,25 @@
+FONT = ("Courier", 24, "normal")
+
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-220, 250)
+        self.update_scoreboard()
+        
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+    
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
+    
